daf9e0db08b1e1d61a4e15623773fd330dda3daf
Steve Davis <steve@celery.club>
2022-10-11 11:53:38 -0700

    Increase resolution of grid canvas

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,31 @@ import World from "./ECS/World";
 import { ComponentKey } from "./ECS/Components";
 import RenderSystem from "./ECS/systems/RenderSystem";
 
+// Device pixel ratio
+const PIXEL_RATIO = devicePixelRatio;
 // Size of each cell
-const CELL_SIZE_PIXELS = 20;
+const CELL_SIZE_PIXELS = 20 * PIXEL_RATIO;
 // Number of cells horizontally
 const GRID_WIDTH = 30;
 // Number of cells vertically
 const GRID_HEIGHT = 30;
-
+// Grid width in pixels
 const GRID_WIDTH_PIXELS = GRID_WIDTH * CELL_SIZE_PIXELS;
+// Grid height in pixels
 const GRID_HEIGHT_PIXELS = GRID_HEIGHT * CELL_SIZE_PIXELS;
 
 const canvas = document.createElement("canvas");
 canvas.width = GRID_WIDTH_PIXELS;
 canvas.height = GRID_HEIGHT_PIXELS;
-canvas.setAttribute("style", `border: 1px solid #aaa; image-rendering: pixelated; width: ${GRID_WIDTH_PIXELS}px;`);
+canvas.setAttribute(
+  "style",
+  `
+    border: 1px solid #ddd;
+    height: ${GRID_HEIGHT_PIXELS / PIXEL_RATIO}px;
+    image-rendering: pixelated;
+    width: ${GRID_WIDTH_PIXELS / PIXEL_RATIO}px;
+  `
+);
 document.body.appendChild(canvas);
 const ctx = canvas.getContext("2d");
 
