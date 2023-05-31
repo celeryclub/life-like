9d928f9a8d74966659a9d487e77948dac4f7f0d9
Steve Davis <steve@celery.club>
2023-05-31 11:52:36 -0700

    Move core and controller initialization into main.ts

--- src/components/x-app.ts
+++ src/components/x-app.ts
@@ -1,16 +1,12 @@
 import { MobxLitElement } from "@adobe/lit-mobx";
-import { World, Layout, Renderer } from "core";
 import { TemplateResult, html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { PIXEL_RATIO, NATURAL_CELL_SIZE, SIDEBAR_WIDTH } from "../Constants";
+import { SIDEBAR_WIDTH } from "../Constants";
 import { ConfigController } from "../game/controllers/ConfigController";
 import { LayoutController } from "../game/controllers/LayoutController";
 import { PlaybackController } from "../game/controllers/PlaybackController";
 import { WorldController } from "../game/controllers/WorldController";
-import { ConfigModel } from "../game/models/ConfigModel";
-import { PlaybackModel } from "../game/models/PlaybackModel";
-import { PluginBuilder } from "../game/plugins/PluginBuilder";
-import { PluginManager, PluginGroup } from "../game/plugins/PluginManager";
+
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import "./x-sidebar";
 
@@ -33,58 +29,28 @@ class App extends MobxLitElement {
     }
   `;
 
-  // Canvas
-  private _canvas = document.getElementById("canvas") as HTMLCanvasElement;
-
-  // Core
-  private _world!: World;
-  private _layout!: Layout;
-  private _renderer!: Renderer;
-
-  // Models
-  private _configModel = new ConfigModel();
-  private _playbackModel = new PlaybackModel();
-
-  // Controllers
-  private _worldController!: WorldController;
-  private _configController!: ConfigController;
-  private _layoutController!: LayoutController;
-  private _playbackController!: PlaybackController;
-
-  // Plugins
-  private _pluginBuilder!: PluginBuilder;
-  private _pluginManager!: PluginManager;
+  @property()
+  public worldController!: WorldController;
 
   @property()
-  public canvas!: HTMLCanvasElement;
+  public configController!: ConfigController;
+
+  @property()
+  public layoutController!: LayoutController;
+
+  @property()
+  public playbackController!: PlaybackController;
 
   constructor() {
     super();
-
-    const context = this._canvas.getContext("2d", { alpha: false })!;
-
-    this._world = World.new();
-    this._layout = Layout.new(this._canvas, PIXEL_RATIO, NATURAL_CELL_SIZE);
-    this._renderer = Renderer.new(context, "lightblue");
-
-    this._worldController = new WorldController(this._world);
-    this._configController = new ConfigController(this._configModel);
-    this._layoutController = new LayoutController(this._canvas, this._layout, this._world, this._renderer);
-    this._playbackController = new PlaybackController(this._layout, this._world, this._playbackModel, this._renderer);
-
-    this._pluginBuilder = new PluginBuilder(this._canvas);
-    this._pluginManager = new PluginManager(this._pluginBuilder, this._layoutController, this._playbackController);
-
-    this._pluginManager.activateGroup(PluginGroup.Default);
-    this._pluginManager.activateGroup(PluginGroup.Playback);
   }
 
   protected render(): TemplateResult {
     return html`<x-sidebar
-      .worldController=${this._worldController}
-      .configController=${this._configController}
-      .layoutController=${this._layoutController}
-      .playbackController=${this._playbackController}
+      .worldController=${this.worldController}
+      .configController=${this.configController}
+      .layoutController=${this.layoutController}
+      .playbackController=${this.playbackController}
     ></x-sidebar> `;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,48 @@
+import { World, Layout, Renderer } from "core";
 import { configure } from "mobx";
+import { PIXEL_RATIO, NATURAL_CELL_SIZE, SIDEBAR_WIDTH } from "./Constants";
+import { ConfigController } from "./game/controllers/ConfigController";
+import { LayoutController } from "./game/controllers/LayoutController";
+import { PlaybackController } from "./game/controllers/PlaybackController";
+import { WorldController } from "./game/controllers/WorldController";
+import { ConfigModel } from "./game/models/ConfigModel";
+import { PlaybackModel } from "./game/models/PlaybackModel";
+import { PluginBuilder } from "./game/plugins/PluginBuilder";
+import { PluginManager, PluginGroup } from "./game/plugins/PluginManager";
 import "./components/x-app";
 
 configure({
   enforceActions: "always",
 });
 
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const context = canvas.getContext("2d", { alpha: false })!;
+
+canvas.style.left = `${SIDEBAR_WIDTH}px`;
+
+const world = World.new();
+const layout = Layout.new(canvas, PIXEL_RATIO, NATURAL_CELL_SIZE);
+const renderer = Renderer.new(context, "lightblue");
+
+const configModel = new ConfigModel();
+const playbackModel = new PlaybackModel();
+
+const worldController = new WorldController(world);
+const configController = new ConfigController(configModel);
+const layoutController = new LayoutController(canvas, layout, world, renderer);
+const playbackController = new PlaybackController(layout, world, playbackModel, renderer);
+
+const pluginBuilder = new PluginBuilder(canvas);
+const pluginManager = new PluginManager(pluginBuilder, layoutController, playbackController);
+
+pluginManager.activateGroup(PluginGroup.Default);
+pluginManager.activateGroup(PluginGroup.Playback);
+
 const app = document.createElement("x-app");
+
+app.worldController = worldController;
+app.configController = configController;
+app.layoutController = layoutController;
+app.playbackController = playbackController;
+
 document.body.appendChild(app);
