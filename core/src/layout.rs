fda8375c4d165d356c6555d6e0967ab481a56470
Steve Davis <steve@celery.club>
2023-06-18 17:52:41 -0700

    Simplify calculations within zoom to fit functionality

--- core/src/layout.rs
+++ core/src/layout.rs
@@ -9,10 +9,10 @@ const MAX_ZOOM_SCALE: f64 = 64.0; // 6400%
 #[wasm_bindgen]
 pub struct Layout {
     canvas: HtmlCanvasElement,
-    pub pixel_ratio: u8, // window.devicePixelRatio
-    pub natural_cell_size: u8,
-    pub offset_x: f64, // Not including pixel ratio
-    pub offset_y: f64, // Not including pixel ratio
+    pub pixel_ratio: u8,       // window.devicePixelRatio
+    pub natural_cell_size: u8, // Cell size at 100% zoom
+    pub offset_x: f64,         // Not including pixel ratio
+    pub offset_y: f64,         // Not including pixel ratio
     pub zoom_scale: f64,
 }
 
@@ -78,19 +78,16 @@ impl Layout {
     pub fn zoom_to_fit(&mut self, world: &World) -> f64 {
         let (world_x, world_y, world_width, world_height) = world.get_bounds();
 
-        let pixel_ratio = self.pixel_ratio as f64;
         let natural_cell_size = self.natural_cell_size as f64;
 
         let natural_world_width = natural_cell_size * world_width as f64;
         let natural_world_height = natural_cell_size * world_height as f64;
 
-        let (canvas_width, canvas_height) = self.get_canvas_size();
+        // We're using the actual canvas size here (not including pixel ratio)
+        let (canvas_width, canvas_height) = self.get_actual_canvas_size();
 
-        let actual_canvas_width = canvas_width as f64 / pixel_ratio;
-        let actual_canvas_height = canvas_height as f64 / pixel_ratio;
-
-        let horizontal_fit_scale = actual_canvas_width / natural_world_width;
-        let vertical_fit_scale = actual_canvas_height / natural_world_height;
+        let horizontal_fit_scale = canvas_width / natural_world_width;
+        let vertical_fit_scale = canvas_height / natural_world_height;
 
         // Use the minimum of horizontal or vertical fit to ensure everything is visible
         let new_zoom_scale = f64::min(horizontal_fit_scale, vertical_fit_scale);
@@ -101,18 +98,18 @@ impl Layout {
         // After the new zoom scale is computed, we can use it to compute the new offset
         let actual_cell_size = natural_cell_size * new_zoom_scale;
 
-        let actual_world_x = pixel_ratio * actual_cell_size * world_x as f64;
-        let actual_world_y = pixel_ratio * actual_cell_size * world_y as f64;
-        let actual_world_width = pixel_ratio * actual_cell_size * world_width as f64;
-        let actual_world_height = pixel_ratio * actual_cell_size * world_height as f64;
+        let actual_world_x = actual_cell_size * world_x as f64;
+        let actual_world_y = actual_cell_size * world_y as f64;
+        let actual_world_width = actual_cell_size * world_width as f64;
+        let actual_world_height = actual_cell_size * world_height as f64;
 
-        let actual_world_center_x = actual_world_x + (actual_world_width / 2.0);
-        let actual_world_center_y = actual_world_y + (actual_world_height / 2.0);
+        let actual_world_center_x = actual_world_x + actual_world_width / 2.0;
+        let actual_world_center_y = actual_world_y + actual_world_height / 2.0;
 
-        // Set offset to the center of the canvas, plus half of the difference
-        // between the world center and true center
-        self.offset_x = (actual_canvas_width) / 2.0 + actual_world_center_x * -0.5;
-        self.offset_y = (actual_canvas_height) / 2.0 + actual_world_center_y * -0.5;
+        // Offset should be the center of the canvas,
+        // plus the difference between the world center and true center
+        self.offset_x = canvas_width / 2.0 + actual_world_center_x * -1.0;
+        self.offset_y = canvas_height / 2.0 + actual_world_center_y * -1.0;
 
         self.zoom_scale = new_zoom_scale;
 
@@ -123,5 +120,14 @@ impl Layout {
 impl Layout {
     pub fn get_canvas_size(&self) -> (u32, u32) {
         (self.canvas.width(), self.canvas.height())
+    }
+
+    pub fn get_actual_canvas_size(&self) -> (f64, f64) {
+        let pixel_ratio = self.pixel_ratio as f64;
+
+        (
+            self.canvas.width() as f64 / pixel_ratio,
+            self.canvas.height() as f64 / pixel_ratio,
+        )
     }
 }
