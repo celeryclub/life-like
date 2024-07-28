f9c2738e9df3283e3950af45c723fddf5a4fa3b7
Steve Davis <steve@celery.club>
2024-07-28 15:34:58 -0700

    Change sidebar width back to 280px

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,3 +1,3 @@
 export const PIXEL_RATIO = window.devicePixelRatio;
 export const NATURAL_CELL_SIZE = 4; // Cell size at 100% zoom
-export const SIDEBAR_WIDTH = 300;
+export const SIDEBAR_WIDTH = 280;
